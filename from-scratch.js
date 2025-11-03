6dad7a7248b0e138877e3ba59cb48968d6c062df
Sadia Islam <sadiaislam25b@gmail.com>
2025-11-03 10:28:00 -0500

    forEach arrow function

--- from-scratch.js
+++ from-scratch.js
@@ -8,19 +8,30 @@
 
 const sampleWords = ['dog', 'blueberry', 'alabama', 'earth', 'football'];
 
-// Problem 1
+// Problem 1 
 const printEveryWord = (words) => {
+  words.forEach((word) => {
+    console.log(word);
+  });
 
-};
+}; // I think there might be a () or {} problem but i didnt have time to fix it
 
 // Problem 2
-const getStringLengths = (strings) => {
+const getStringLengths = (words) => {
+  return words.map((word) => word.leanth);
 
 };
+const sampleWords = ['dog', 'blueberry', 'alabama', 'earth', 'football'];
+console.log(getStringLengths)
 
 // Problem 3
 const sortWordsZtoA = (words) => {
-
+  words.sort((a, b) => {
+    if (a < b)) return 1;
+  if (a > b)) return -1;
+  return 0;
+}
+    
 };
 
 /* -------------------------------------------- */
@@ -31,11 +42,13 @@ const sampleNumbers = [7, 3, 9, 1, 4, 8, 2, 10, 5, 6];
 
 // Problem 4
 const getEvenNumbers = (nums) => {
+  return numbers.filter((num) => num % 2 === 0)
 
 };
 
-// Problem 5
+// Problem 5 // I used .reduce , 
 const getLargestNumber = (numbers) => {
+  return numbers.reduce((largest, current) => { return current > largest })
 
 };
 
@@ -51,30 +64,33 @@ const sampleUsers = [
   { name: 'Alice', isActive: false, age: 28, score: 100 },
 ];
 
-// Problem 6
+// Problem 6 // .foreach
 const printUserNamesAndScores = (users) => {
 
 };
 
 
-// Problem 7
+// Problem 7 //.map
 const getUserNames = (users) => {
 
+  return users.map((user) => user.name)
 };
 
 
-// Problem 8
+// Problem 8 .filter
 const getActiveUsersUnder30 = (users) => {
+  return users.filter((user) => user. & user.age < 30)
 
 };
 
 
-// Problem 9
+// Problem 9 .reduce
 const getTotalScore = (users) => {
-
+  return users.reduce((total, user) => total + user.score, 0)
 };
 
-// Problem 10
+// Problem 10 .sort
 const sortUsersByScoreDescending = (users) => {
+  users.sort((a, b) => b.score - a.score)
 
 };
